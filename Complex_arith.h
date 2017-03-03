fd5e562e83c588b84a8e01453368c132f4339809
akrolla <akrolla@users.noreply.github.com>
2017-03-03 01:29:11 +0000

    Update Complex_arith.h

--- Complex_arith.h
+++ Complex_arith.h
@@ -1,1 +1,36 @@
+ #ifndef ADDCOMPLEX_H
+ #define ADDCOMPLEX_H
+ complex add_complex(complex, complex);
+ #endif
+
+
+#ifndef NEGATE_H
+#define NEGATE_H
+complex negate_complex(complex, complex);
+#endif
+
+#ifndef DIVIDE_H
+#define NEGATE_H
+void divide_complex(complex, complex);
+#endif
+
+
+
+#ifndef MULTIPLY_H
+#define MULTIPLY_H
+complex multiply_complex(complex, complex);
+#endif
+
+  
+#ifndef CONJUGATE_H
+#define CONJUGATE_H
+complex conjugate_complex(complex);
+#endif
+
+#ifndef SUBTRACT_H
+#define SUBTRACT_H
+complex sub_complex(complex,complex);
+#endif
+
+
 
