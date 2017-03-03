8dca6a929ba8e4f500e98ac320fec2fb0e03f021
akrolla <akrolla@users.noreply.github.com>
2017-03-03 11:03:22 -0600

    Update Complex_IO.c

--- Complex_IO.c
+++ Complex_IO.c
@@ -11,9 +11,5 @@ return num;
 
 void write_complex(complex)
 {
-complex num;
-printf("Enter a Real, followed by an Imaginary Number:\n")
-scanf("%d %d ", &num.real , &num.imag);
-printf("%d%di", &num.real , &num.imag);
-return num;
+printf("%d + %di", num.real , num.imag);
 }
